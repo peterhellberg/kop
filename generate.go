07772c4c87d5547d0c6319059c9ec42e1715cf35
Peter Hellberg <peter@c7.se>
2023-09-25 23:14:41 +0200

    generate: Remove rm -rf directive

--- generate.go
+++ generate.go
@@ -1,6 +1,5 @@
 package kop
 
-//go:generate rm -rf ./rpc
-//go:generate mkdir ./rpc
+//go:generate mkdir -p ./rpc
 //go:generate go run github.com/pacedotdev/oto@v0.14.2 -template template.plush -out ./rpc/gen.go -pkg rpc ./definitions
 //go:generate goimports -w ./rpc/gen.go ./rpc/gen.go
