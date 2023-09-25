ca13d0ed8eb6d0c74f9e95b4549c9a6ddc629278
Peter Hellberg <peter@c7.se>
2023-09-25 19:28:46 +0200

    gen: Initial go:generate directives

---
+++ generate.go
@@ -0,0 +1,6 @@
+package kop
+
+//go:generate rm -rf ./rpc
+//go:generate mkdir ./rpc
+//go:generate go run github.com/pacedotdev/oto@v0.14.2 -template template.plush -out ./rpc/gen.go -pkg rpc ./definitions
+//go:generate goimports -w ./rpc/gen.go ./rpc/gen.go
