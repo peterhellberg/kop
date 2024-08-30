133522df72f069f5e15a157bb8a9c87a9f595cbc
Peter Hellberg <peter@c7.se>
2024-08-30 13:22:25 +0200

    generate: bump oto version to v0.20.0

--- generate.go
+++ generate.go
@@ -1,5 +1,5 @@
 package kop
 
 //go:generate mkdir -p ./rpc
-//go:generate go run github.com/pacedotdev/oto@v0.14.2 -template template.plush -out ./rpc/gen.go -pkg rpc ./definitions
+//go:generate go run github.com/pacedotdev/oto@v0.20.0 -template template.plush -out ./rpc/gen.go -pkg rpc ./definitions
 //go:generate goimports -w ./rpc/gen.go ./rpc/gen.go
